b1d0bbfe378e2537f392b29471903b7792938ee7
shahidul shohid <shahidulislamshohid7gmail.com>
2024-12-22 11:53:08 +0600

    server side setup is done

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const port = process.env.PORT || 800
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const port = process.env.PORT || 8000
+
+const app = express()
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Volunteer management server is running')
+})
+
+app.listen(port, ()=> {
+    console.log(`Volunteer management server is running on PORT: ${port}`)
+})
