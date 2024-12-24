e3b7692a7c2203f840fbfcf5055a9463091c1f34
shahidul shohid <shahidulislamshohid7gmail.com>
2024-12-24 17:56:04 +0600

    update method is done

--- index.js
+++ index.js
@@ -95,9 +95,21 @@ async function run() {
         const id = req.params.id 
         const query = {_id: new ObjectId(id)}
         const result = await volunteerManagementCollection.deleteOne(query)
-        console.log(result)
         res.send(result)
 
+      })
+
+      //update data for manageMyProfile 
+      app.put('/update-manage-profile/:id', async(req, res) => {
+        const id = req.params.id 
+        const query = {_id: new ObjectId(id)}
+        const updateData = req.body
+        const update = {
+          $set: updateData,
+        }
+        const options = {upsert:true}
+        const result = await volunteerManagementCollection.updateOne(query, update, options)
+        res.send(result)
       })
 
 
